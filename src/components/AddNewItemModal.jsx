11264e9b90ffe00c3e5430ad2afb7f469a7ef886
tabdurahmon <dilsozxonxatamova@gmail.com>
2024-09-16 07:51:29 +0500

    add

--- src/components/AddNewItemModal.jsx
+++ src/components/AddNewItemModal.jsx
@@ -6,7 +6,7 @@ import {
 } from "@/components/ui/dialog";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/Button";
+import Button from "../ui/Button";
 
 import { getFormData } from "../lib/my-utils/index";
 import { useAppStore } from "../lib/zustand";
